8f0e3df3586496d4495119485abe4eae86afa321
kyle <kyleoneill24@gmail.com>
2021-01-19 16:26:14 -0600

    style change for frontend

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
-//import './semantic.cyborg.min.css';
-import 'semantic-ui-css/semantic.min.css'
+import './semantic.darkly.min.css';
+//import 'semantic-ui-css/semantic.min.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
